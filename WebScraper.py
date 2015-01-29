60b5ad388e10f8785679f6ef385fef97dc8b305a
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-01-30 03:33:04 +0800

    Reading table from html

--- WebScraper.py
+++ WebScraper.py
@@ -2,17 +2,26 @@ from bs4 import BeautifulSoup
 import re
 import urllib
 
+
+output = open("testoutput.txt","w")
+
 url_a = "http://www.basketball-reference.com/players/a/"
 
 html = urllib.urlopen(url_a).read();
 soup = BeautifulSoup(html)
 
 player_table = soup.findAll("table",{"class":"sortable stats_table" })
-print player_table
+#print player_table
 
-#each_row = player_table.find_all('tr')
-'''
-for er in each_row:
+for each_row in player_table:
+	row = each_row.find_all('tr')
+	for each_element in row:
+			cell = each_element.find_all('td')
+			#output.write(cell)
+			print cell[0].text+","
+
+
+'''for er in each_row:
 	tds = er.findAll('td')
 	print (tds[0].text)+","+(tds[1],text)
 '''
