a9c9cd058619482b80e2fc3d27d097174f758589
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-02-01 12:50:15 +0800

    Getting active player records

--- WebScraper.py
+++ WebScraper.py
@@ -2,13 +2,10 @@ from bs4 import BeautifulSoup
 import re
 import urllib
 
-
 output = open("testoutput.txt","w")
 csv_output = open("csv_output.txt","w")
 
-
 url_a = "http://www.basketball-reference.com/players/a/"
-
 html = urllib.urlopen(url_a).read();
 soup = BeautifulSoup(html)
 
@@ -17,11 +14,29 @@ player_table = soup.find("table",{"class":"sortable stats_table" })
 #output.write(player_table.__str__())
 
 get_rows = player_table.find_all('tr')
+get_rows_custom = []
+print get_rows[0].__str__()
+
+##Getting only the rows where name is "<Strong>"
+needle = "<strong>"
+for i in range(len(get_rows)):
+	if(re.search(needle,get_rows[i].__str__()) or i == 0):
+		get_rows_custom.append(get_rows[i])
+
+##
+
+print "get_rows_custom : "
+print get_rows_custom
+print "type(get_rows_custom) : " 
+print type(get_rows_custom)
+
 all_rows = []
 all_data = []
 all_text = []
-for each_row in get_rows:
+for each_row in get_rows_custom:
+	#text = ''.join(each_row.find_all(text=True and 'strong'))
 	text = ''.join(each_row.find_all(text=True))
+	#print text
 	single_row = (each_row.find_all(text=True))
 	all_rows.append(single_row)
 	all_data.append(text)
@@ -30,15 +45,7 @@ for each_row in get_rows:
 output.write(all_data.__str__())
 
 
-print all_data[1].__str__()
-print all_data[2].__str__()
-print len(all_data)
-
-if all_data[1][68].__str__() == '\n\n':
-	print "It matches!!"
-
 #Function to parse table into CSV format
-
 def tableToCSV():
 	print "Converting table to CSV..."
 	table_data=""
@@ -55,5 +62,22 @@ def tableToCSV():
 		csv_output.write("\n")
 		table_data=""
 
-
 tableToCSV()
+
+
+
+##Getting href for individual letters
+print "\n\n\n*****************"
+home_url = "http://www.basketball-reference.com/players/"
+home_html = urllib.urlopen(home_url).read();
+soup = BeautifulSoup(home_html)
+get_alphabet_section = soup.find_all("div",{"id":"page_content"})
+output1 = open("testoutput1.txt","w")
+output1.write(get_alphabet_section.__str__())
+'''
+for url in soup.find_all('a',href=True):
+	print "Following URLs found :", url['href'],"\n"
+'''
+
+
+
