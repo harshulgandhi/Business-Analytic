80b525240975c24da671e38e6506288d497e8470
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-02-01 20:32:26 +0800

    Individual player's page parsing and saving stats

--- WebScraper.py
+++ WebScraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import re
 import urllib
+import os
+import time
 
 output = open("testoutput.txt","w")
 csv_output = open("csv_output.txt","w")
@@ -40,31 +42,33 @@ def getPlayerBasicInformation(link):
 	print "\nCompiled URL is ",url,"\n"
 	html = urllib.urlopen(url).read();
 	soup = BeautifulSoup(html)
-	player_table = soup.find("table",{"class":"sortable stats_table" })
+	player_table = soup.find("table",{"class":"sortable stats_table" }) 	#reading the players table
 	#print player_table.__str__()
 	#output.write(player_table.__str__())
-	get_rows = player_table.find_all('tr')
+	get_rows = player_table.find_all('tr')		#Getting individual rows
 	get_rows_custom = []
+	player_page_url=[]
 	#print get_rows[0].__str__()
 
 	##Getting only the rows where name is "<Strong>"
-	needle = "<strong>"
+	needle_0= "<strong>"
+	needle_1= '<a href="(/players/.*)">'
 	for i in range(len(get_rows)):
-		if(re.search(needle,get_rows[i].__str__()) or i == 0):
+		if(re.search(needle_0,get_rows[i].__str__()) or i == 0):		#Looking for rows containing "strong" tag
+			if i > 0:
+				haystack = get_rows[i].__str__()
+				result = re.search(needle_1,haystack)
+				player_page_url.append(result.group(1))
+				print result.group(1)									#This gives individual player url
+																		#Function call to read, parse and save individual player's data
 			get_rows_custom.append(get_rows[i])
+			#output.write(get_rows_custom.__str__())
 
-	##
-	'''
-	print "get_rows_custom : "
-	print get_rows_custom
-	print "type(get_rows_custom) : " 
-	print type(get_rows_custom)
-	'''
-	
 	all_data = []
 	all_text = []
 	for each_row in get_rows_custom:
 		text = ''.join(each_row.find_all(text=True))
+		#print player_url[0]
 		single_row = (each_row.find_all(text=True))
 		all_data.append(text)
 		
@@ -77,10 +81,11 @@ def getPlayerBasicInformation(link):
 #Function to parse table into CSV format
 def tableToCSV(all_data,link):
 	print "Converting table to CSV...\n"
-	needle="/(.?)/"
+	path = '/basic profile'
+	needle="/(.?)/"												#Getting alphabet for which tables is being converted
 	result = re.search(needle,link)
 	alphabet = result.group(1)
-	nameOfFile = "csv_output_for_"+alphabet+".txt"
+	nameOfFile = os.path.join("basic profile/","csv_output_for_"+alphabet+".txt")
 	csv_output = open(nameOfFile,"w")
 	table_data=""
 	tableSize = len(all_data)
@@ -103,11 +108,61 @@ def tableToCSV(all_data,link):
 links_list = getAlphabetLinks()
 i = 0
 for link in links_list:
-	if(i==25):
+	if(i==2):
 		break
+	print link['href']
 	all_data,link1 = getPlayerBasicInformation(link['href'])
 	tableToCSV(all_data,link1)
 	i+=1
 
 print "Players basic information saved!!"
+
+
+
+##### Reading individual player's page and saving all stats#####
+player_name="machasc01"
+particular_player_url="http://www.basketball-reference.com/players/m/machasc01.html"
+
+#player_name="acyqu01"
+#particular_player_url = "http://www.basketball-reference.com/players/a/acyqu01.html"
+
+player_html = urllib.urlopen(particular_player_url).read();
+soup1 = BeautifulSoup(player_html)
+print "Reading page"
+get_stat_tables = soup1.find_all("div",{"class":"stw"})
+
+testoutput2 = open("testoutput2.txt","w")
+testoutput2.write(get_stat_tables.__str__())
+needle_2 = '<div class="stw" id="(all_.*)">'
+
+testoutput3 = open("testoutput3.txt","w")
+csv_output1=open("stat_csv_table.txt","w")
+
+result = re.findall(needle_2,get_stat_tables.__str__())			#List of all tables on this page
+for i in range(len(result)):
+	print "****Fetching table",result[i],"****"
+	table_data=""
+	stat_table_data=[]
+	ind_table = soup1.find("div",{"id":result[i]})			#result[i] is the table name 'all_*'
+	#testoutput3.write(ind_table.__str__())
+	get_rows = ind_table.find_all('tr')
+	for each_row in get_rows:
+		text = ''.join(each_row.find_all(text=True))
+		stat_table_data.append(text)
+	filename= "player_stat_"+player_name+"_"+result[i]+".txt"
+	csv_stat_output = open(filename,"w")
+	tableSize = len(stat_table_data)
+	for i in range(0,tableSize,1):
+		row_len = len(stat_table_data[i])
+		for j in range(1,row_len,1):
+			if stat_table_data[i][j].__str__()!='\n':
+				table_data = table_data + stat_table_data[i][j]
+			else:
+				table_data = table_data + ","
+		
+
+		csv_stat_output.write(table_data)	
+		csv_stat_output.write("\n")
+		table_data=""
+
 
